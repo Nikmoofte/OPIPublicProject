4e9130b1dc5b6390dab981e8f78433877149e267
Nikmoofte <ilya.vailov@gmail.com>
2021-11-21 22:06:11 +0300

    Trying to make in work

---
+++ src/main/Tests/com/project/bakkara/baccaraControllerTest.java
@@ -0,0 +1,50 @@
+package com.project.bakkara;
+
+import static org.junit.Assert.*;
+
+public class baccaraControllerTest {
+
+    @org.junit.Test
+    public void lastDefine() {
+    }
+
+    @org.junit.Test
+    public void declareDraw() {
+    }
+
+    @org.junit.Test
+    public void declarePlWin() {
+    }
+
+    @org.junit.Test
+    public void declareOppWin() {
+    }
+
+    @org.junit.Test
+    public void resetButtons() {
+    }
+
+    @org.junit.Test
+    public void clear() {
+    }
+
+    @org.junit.Test
+    public void generateCard() {
+    }
+
+    @org.junit.Test
+    public void scoreUpdate() {
+    }
+
+    @org.junit.Test
+    public void play() {
+    }
+
+    @org.junit.Test
+    public void pass() {
+    }
+
+    @org.junit.Test
+    public void anotherOne() {
+    }
+}
